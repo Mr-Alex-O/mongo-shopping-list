067dbc47f9f7c92248b76dd366c61bfd86b28af9
Alex <alexolszewski1993@gmail.com>
2017-04-15 03:08:40 +0000

    hide key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                           'mongodb://Alex:pr0gramming@ds161640.mlab.com:61640/heroku_qwcdj1hk' :
+                            'mongodb://Alex:pr0gramming@ds161640.mlab.com:61640/heroku_qwcdj1hk');
 exports.PORT = process.env.PORT || 8080;
--- test/test-server.js
+++ test/test-server.js
@@ -12,41 +12,88 @@ var app = server.app;
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
-    before(function(done) {
-        server.runServer(function() {
-            Item.create({name: 'Broad beans'},
-                        {name: 'Tomatoes'},
-                        {name: 'Peppers'},
-                        {name: 'Bread'}, function() {
-                done();
-            });
-        });
-    });
-
-    after(function(done) {
-        Item.remove(function() {
-            done();
-        });
-    });
-    
-    it('should list items on get', function(done) {
-            chai.request(app)
-                .get('/items')
-                .end(function(err, res) {
-                    should.equal(err, null);
-                    res.should.have.status(200);
-                    res.body.should.have.length(4);
-                    
-                    chai.request(app)
-                    .get('/items/'+ res.body[0]._id)
-                    .end(function(err, res){
-                        should.equal(err, null);
-                        res.should.have.status(200);
-                        res.body.name.should.equal('Broad beans');
+            before(function(done) {
+                server.runServer(function() {
+                    Item.create({
+                        name: 'Broad beans'
+                    }, {
+                        name: 'Tomatoes'
+                    }, {
+                        name: 'Peppers'
+                    }, {
+                        name: 'Bread'
+                    }, function() {
                         done();
                     });
                 });
-        });
-        
-        
-});
+            });
+
+            after(function(done) {
+                Item.remove(function() {
+                    done();
+                });
+            });
+
+
+            describe('Thinkful Examples', function() {
+                it('should list items on get', function(done) {
+                    chai.request(app)
+                        .get('/items')
+                        .end(function(err, res) {
+                            should.equal(err, null);
+                            res.should.have.status(200);
+                            res.body.should.have.length(3);
+
+                            chai.request(app)
+                                .get('/items/' + res.body[0]._id)
+                                .end(function(err, res) {
+                                    should.equal(err, null);
+                                    res.should.have.status(200);
+                                    res.body.name.should.equal('Broad beans');
+                                    done();
+                                });
+                        });
+                });
+
+                // it('should edit an item on PUT', function(done) {
+
+                //     chai.request(app)
+                //         .get('/items')
+                //         .end(function(err, res) {
+                //             chai.request(app)
+                //                 .put('/items/' + res.body[0].id)
+                //                 .send({
+                //                     _id: res.body[0].id
+                //                 }, {
+                //                     'name': 'Beer'
+                //                 })
+                //                 .end(function(err, response) {
+                //                     response.should.have.status(204);
+                //                     done();
+                //                 })
+                //         })
+                // });
+                it('should Delete an item', function(done) {
+                    chai.request(app)
+                        .get('/items')
+                        .end(function(err, res) {
+                            chai.request(app)
+                                .delete('/items/' + res.body[0]._id)
+                                .end(function(error, response1) {
+                                    response1.should.have.status(204);
+
+                                    chai.request(app)
+                                        .get('/items')
+                                        .end(function(error, response) {
+                                            console.log(response.body);
+                                            console.log(res.body[0]._id);
+                                            should.equal(err, null);
+
+                                            done();
+                                        });
+
+                                });
+                        });
+                });
+
+            });
